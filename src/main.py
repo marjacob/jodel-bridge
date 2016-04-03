e13f72bc565a8de1f10a537c0a15d6ff9655d446
Martin Røed Jacobsen <marjacob@users.noreply.github.com>
2016-04-03 18:38:20 +0200

    Added definitions from the Jodel API

--- src/main.py
+++ src/main.py
@@ -2,21 +2,8 @@
 # -*- coding: utf-8 -*-
 
 
-import datetime
 import os
 import sys
-
-
-def unix_time_millis(dt):
-    """
-    http://stackoverflow.com/a/11111177
-    """
-    epoch = datetime.datetime.utcfromtimestamp(0)
-    return (dt - epoch).total_seconds() * 1000.0
-
-
-def hmac():
-    now = unix_time_millis(datetime.datetime.utcnow())
 
 
 def main(args):
