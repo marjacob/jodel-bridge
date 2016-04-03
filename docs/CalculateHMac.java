68a7071e662deeb30d5bee5a73a45875001047d8
Martin Røed Jacobsen <marjacob@users.noreply.github.com>
2016-04-03 15:27:48 +0200

    Added more information about the HMAC generation

--- docs/CalculateHMac.java
+++ docs/CalculateHMac.java
@@ -1,25 +1,33 @@
 private String calculateHMac(Request paramRequest, String paramString) {
 	try {
+		/* This may be either the complete request URL or a partial URL.
+		 * Not known as of yet due to obfuscation. */
 		URI localURI = paramRequest.BT();
+
+		/* Create a StringBuilder and initialise it to the HTTP method.
+		 * paramRequest.Cw() may be "GET", "POST", "PUT", etc. */
 		StringBuilder localStringBuilder = new StringBuilder(paramRequest.Cw());
 
+		/* Append some information about the request separated by '%' signs. */
 		localStringBuilder
 			.append("%")
-			.append(localURI.getHost())
+			.append(localURI.getHost())        /* "api.go-tellm.com" */
 			.append("%")
-			.append(String.valueOf(this.port))
+			.append(String.valueOf(this.port)) /* "443" */
 			.append("%")
-			.append(localURI.getPath())
+			.append(localURI.getPath())        /* "/api/v2/users/" */
 			.append("%");
 
 		/* Try to get the "Authorization" header from the request. */
 		String str = paramRequest.dn("Authorization");
 
-		/* Append the "Authorization" header if it was found.  */
+		/* Append the contents of the "Authorization" header if it was found. */
 		if (!TextUtils.isEmpty(str)) {
 			localStringBuilder.append(str.split(" ")[1]);
 		}
-		/* Append the current time formatted as ISO 8601 with the offset from UTC.*/
+		/* Append the current time formatted as ISO 8601 with the offset from UTC.
+		 *   - Format : "YYYY-MM-DDTHH:MM:SSZ"
+		 *   - Example: "2016-04-03T13:12:38Z" */
 		localStringBuilder
 			.append("%")
 			.append(paramString);
